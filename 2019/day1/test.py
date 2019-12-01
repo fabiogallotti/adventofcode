a33b34a741fe210770d7f9bcb48f096fcc191ad7
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2019-12-01 19:20:25 +0100

    Tests

---
+++ 2019/day1/test.py
@@ -0,0 +1,13 @@
+from utils import fuel_computation, fuel_recursive
+
+def test_fuel_computation():
+    assert fuel_computation(12) == 2
+    assert fuel_computation(14) == 2
+    assert fuel_computation(1969) == 654
+    assert fuel_computation(100756) == 33583
+
+
+def test_fuel_recursive(): 
+    assert fuel_recursive(14) == 2
+    assert fuel_recursive(1969) == 966
+    assert fuel_recursive(100756) == 50346
