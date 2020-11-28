2eb24a0a39c470cd18093a7db758a12e9d87271d
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2020-11-28 16:37:32 +0100

    2015day6 part 2

--- src/year2015/day6/exercise2.py
+++ src/year2015/day6/exercise2.py
@@ -1,9 +1,9 @@
 from functions.read_input import read_multiple_lines_separated
 from inputs.path import PATH
-from .functions import calculate_lights_on
+from .functions import calculate_brightness
 
 data = read_multiple_lines_separated(f"{PATH}/2015day6.txt", " ")
 
 lights = [[0 for row in range(1000)] for column in range(1000)]
 
-print(calculate_lights_on(data, lights))
+print(calculate_brightness(data, lights))
