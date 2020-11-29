f4a56568e4c0979798ec40f74346794a3c4f53fe
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2020-11-29 18:05:13 +0100

    improvements

--- src/year2015/day7/functions.py
+++ src/year2015/day7/functions.py
@@ -14,14 +14,13 @@ def bitwise_not(elem):
     return -(~int(elem) ^ 65535) - 1
 
 
-def assign_value(wires, wire, instruction):
-    if type(wires[instruction[0]]) == int:
-        wires[wire] = int(wires[instruction[0]])
+def assign_values(wires, wire, instruction, operation):
+    if check_int(wires[instruction]):
+        wires[wire] = operation(wires[instruction])
 
 
-def check_not(wires, wire, instruction):
-    if type(wires[instruction[1]]) == int:
-        wires[wire] = bitwise_not(wires[instruction[1]])
+def check_int(elem):
+    return type(elem) == int
 
 
 def check_operations(wires, wire, instruction, operation):
@@ -34,15 +33,15 @@ def check_operations(wires, wire, instruction, operation):
     }
 
     try:
-        if instruction[0].isnumeric() and type(wires[instruction[2]]) == int:
+        if instruction[0].isnumeric() and check_int(wires[instruction[2]]):
             wires[wire] = operators[operation](
                 int(instruction[0]), int(wires[instruction[2]])
             )
-        elif type(wires[instruction[0]]) == int and instruction[2].isnumeric():
+        elif check_int(wires[instruction[0]]) and instruction[2].isnumeric():
             wires[wire] = operators[operation](
                 int(wires[instruction[0]]), int(instruction[2])
             )
-        elif type(wires[instruction[0]]) == int and type(wires[instruction[2]]) == int:
+        elif check_int(wires[instruction[0]]) and check_int(wires[instruction[2]]):
             wires[wire] = operators[operation](
                 int(wires[instruction[0]]), int(wires[instruction[2]])
             )
@@ -50,28 +49,31 @@ def check_operations(wires, wire, instruction, operation):
         pass
 
 
+def decisions(wires, wire, instruction):
+    if instruction[0] == "NOT":
+        assign_values(wires, wire, instruction[1], bitwise_not)
+
+    elif instruction[1] == "AND":
+        check_operations(wires, wire, instruction, "AND")
+
+    elif instruction[1] == "OR":
+        check_operations(wires, wire, instruction, "OR")
+
+    elif instruction[1] == "LSHIFT":
+        check_operations(wires, wire, instruction, "LSHIFT")
+
+    elif instruction[1] == "RSHIFT":
+        check_operations(wires, wire, instruction, "RSHIFT")
+
+
 def emulate_circuit(wires, stopping_condition):
     while True:
         for wire, instruction in wires.items():
-            if type(instruction) is not int:
+            if not check_int(instruction):
                 if len(instruction) > 1:
-                    if instruction[0] == "NOT":
-                        check_not(wires, wire, instruction)
-
-                    elif instruction[1] == "AND":
-                        check_operations(wires, wire, instruction, "AND")
-
-                    elif instruction[1] == "OR":
-                        check_operations(wires, wire, instruction, "OR")
-
-                    elif instruction[1] == "LSHIFT":
-                        check_operations(wires, wire, instruction, "LSHIFT")
-
-                    elif instruction[1] == "RSHIFT":
-                        check_operations(wires, wire, instruction, "RSHIFT")
-
+                    decisions(wires, wire, instruction)
                 elif len(instruction) == 1:
-                    assign_value(wires, wire, instruction)
+                    assign_values(wires, wire, instruction[0], int)
 
         if type(wires[stopping_condition]) is int:
             break
