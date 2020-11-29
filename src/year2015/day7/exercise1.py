66d018abfb824b087ad13ecb69d54fc71cc4499f
fabiogallotti <42519154+fabiogallotti@users.noreply.github.com>
2020-11-29 15:40:14 +0100

    2015 day7 (#11)

---
+++ src/year2015/day7/exercise1.py
@@ -0,0 +1,11 @@
+from functions.read_input import read_multiple_lines_separated
+from inputs.path import PATH
+from .functions import emulate_circuit, preprocessing
+
+data = read_multiple_lines_separated(f"{PATH}/2015day7.txt", " ")
+
+wires = preprocessing(data)
+
+emulate_circuit(wires)
+
+print(wires["a"])
