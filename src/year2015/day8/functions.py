ac7a29a54129dd34efeee55ffd23cebcba5b1f75
fabiogallotti <fabiogallotti@users.noreply.github.com>
2020-11-30 19:09:48 +0000

    format code with black

--- src/year2015/day8/functions.py
+++ src/year2015/day8/functions.py
@@ -9,6 +9,7 @@ def difference_string_memory(data):
 def remove_double_quotes(elem):
     return elem[1 : len(elem) - 1]
 
+
 def memory_representation_length(data):
     memory_length = 0
     for elem in data:
@@ -28,15 +29,17 @@ def memory_representation_length(data):
                 i += 1
     return memory_length
 
+
 def new_representation_length(data):
     new_length = 0
     for elem in data:
         for i in range(1, len(elem[0]) + 1):
-            if elem[0][i - 1] in ["\\", "\""]:
-                new_length +=1
+            if elem[0][i - 1] in ["\\", '"']:
+                new_length += 1
             new_length += 1
-        new_length+=2
+        new_length += 2
     return new_length
+
 
 def difference_new_string(data):
     return new_representation_length(data) - string_representation_length(data)
--- tests/2015/test_day8.py
+++ tests/2015/test_day8.py
@@ -1,8 +1,8 @@
 from src.year2015.day8.functions import (
-    string_representation_length,
-    memory_representation_length,
-    difference_string_memory,
     difference_new_string,
+    difference_string_memory,
+    memory_representation_length,
+    string_representation_length,
 )
 
 
@@ -19,6 +19,7 @@ def test_memory_representation_length():
 def test_difference_string_memory():
     input = [['""'], ['"abc"'], ['"aaa\\"aaa"'], ['"\\x27"']]
     assert difference_string_memory(input) == 12
+
 
 def test_difference_new_memory():
     input = [['""'], ['"abc"'], ['"aaa\\"aaa"'], ['"\\x27"']]
