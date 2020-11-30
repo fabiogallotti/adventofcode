909b5db7b26dcd794ac1420d6e018e0ac18f9968
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2020-11-30 20:17:12 +0100

    2020 day1

--- src/year2015/day8/exercise1.py
+++ src/year2015/day8/exercise1.py
@@ -1,8 +1,8 @@
-from functions.read_input import read_multiple_lines_separated
+from functions.read_input import read_multiple_lines
 from inputs.path import PATH
 
 from .functions import difference_string_memory
 
-data = read_multiple_lines_separated(f"{PATH}/2015day8.txt", " ")
+data = read_multiple_lines(f"{PATH}/2015day8.txt")
 
 print(difference_string_memory(data))
