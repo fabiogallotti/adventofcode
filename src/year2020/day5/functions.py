0bc6a992c840fcd476aff26658b1a32b6ea8be7f
fabiogallotti <fabiogallotti@users.noreply.github.com>
2020-12-05 08:03:33 +0000

    format code with black

--- src/year2020/day5/functions.py
+++ src/year2020/day5/functions.py
@@ -7,22 +7,23 @@ def find_seat_ids(data):
 
         for char in elem[:7]:
             if char == "F":
-                rows = rows[ : len(rows)//2]
+                rows = rows[: len(rows) // 2]
             elif char == "B":
-                rows = rows[len(rows)//2 : ]
+                rows = rows[len(rows) // 2 :]
 
         for char in elem[7:]:
             if char == "L":
-                columns = columns[ : len(columns)//2]
+                columns = columns[: len(columns) // 2]
             elif char == "R":
-                columns = columns[len(columns)//2 : ]
+                columns = columns[len(columns) // 2 :]
 
-        seat_ids.append(rows[0]*8 + columns[0])
+        seat_ids.append(rows[0] * 8 + columns[0])
     return seat_ids
+
 
 def find_missing_seat_id(seat_ids):
     seat_ids.sort()
 
     for i, elem in enumerate(seat_ids):
-        if seat_ids[i+1] - elem != 1:
-            return elem+1
+        if seat_ids[i + 1] - elem != 1:
+            return elem + 1
