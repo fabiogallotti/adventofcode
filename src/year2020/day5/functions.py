3ccbbc3bd63885690984884f331da0bc4d158881
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2020-12-05 09:26:00 +0100

    improvement 2020day5

--- src/year2020/day5/functions.py
+++ src/year2020/day5/functions.py
@@ -1,24 +1,25 @@
+def search_seat_id(elem):
+    rows = range(128)
+    columns = range(8)
+
+    for char in elem[:7]:
+        rows = binary_search(char, "F", "B", rows)
+    for char in elem[7:]:
+        columns = binary_search(char, "L", "R", columns)
+
+    return rows[0] * 8 + columns[0]
+
+
+def binary_search(char, left, right, data):
+    if char == left:
+        data = data[: len(data) // 2]
+    elif char == right:
+        data = data[len(data) // 2 :]
+    return data
+
+
 def find_seat_ids(data):
-    seat_ids = []
-
-    for elem in data:
-        rows = range(128)
-        columns = range(8)
-
-        for char in elem[:7]:
-            if char == "F":
-                rows = rows[: len(rows) // 2]
-            elif char == "B":
-                rows = rows[len(rows) // 2 :]
-
-        for char in elem[7:]:
-            if char == "L":
-                columns = columns[: len(columns) // 2]
-            elif char == "R":
-                columns = columns[len(columns) // 2 :]
-
-        seat_ids.append(rows[0] * 8 + columns[0])
-    return seat_ids
+    return [search_seat_id(elem) for elem in data]
 
 
 def find_missing_seat_id(seat_ids):
