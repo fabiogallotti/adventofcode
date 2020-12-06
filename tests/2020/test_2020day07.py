10f565543fac847cca4701f3d95bd786686a4277
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2020-12-06 21:52:44 +0100

    2020 day7

---
+++ tests/2020/test_2020day07.py
@@ -0,0 +1,54 @@
+from src.year2020.day07.functions import preprocessing, number_containing_bags, bag_size
+
+
+def test_preprocessing():
+    assert preprocessing(
+        ["light red bags contain 1 bright white bag, 2 muted yellow bags."]
+    ) == {"light_red": {"bright_white": 1, "muted_yellow": 2}}
+
+
+def test_number_containing_bags():
+    data = [
+        "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+        "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+        "bright white bags contain 1 shiny gold bag.",
+        "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+        "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+        "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+        "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+        "faded blue bags contain no other bags.",
+        "dotted black bags contain no other bags.",
+    ]
+    rules = preprocessing(data)
+    assert number_containing_bags(["shiny_gold"], rules) == 4
+
+
+def test_bag_size():
+    data = [
+        "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+        "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+        "bright white bags contain 1 shiny gold bag.",
+        "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+        "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+        "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+        "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+        "faded blue bags contain no other bags.",
+        "dotted black bags contain no other bags.",
+    ]
+    rules = preprocessing(data)
+    assert bag_size("shiny_gold", rules) == 32
+
+
+def test_bag_size2():
+    data = [
+        "shiny gold bags contain 2 dark red bags.",
+        "dark red bags contain 2 dark orange bags.",
+        "dark orange bags contain 2 dark yellow bags.",
+        "dark yellow bags contain 2 dark green bags.",
+        "dark green bags contain 2 dark blue bags.",
+        "dark blue bags contain 2 dark violet bags.",
+        "dark violet bags contain no other bags.",
+    ]
+
+    rules = preprocessing(data)
+    assert bag_size("shiny_gold", rules) == 126
