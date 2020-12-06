f44af82c04800e29fd2619353e12574b9854cec2
fabiogallotti <42519154+fabiogallotti@users.noreply.github.com>
2020-12-06 23:04:26 +0100

    refactoring (#53)

--- src/year2019/day04/functions.py
+++ src/year2019/day04/functions.py
@@ -6,25 +6,23 @@ def check_no_decrease(data):
     return all(data[i] <= data[i + 1] for i in range(5))
 
 
+def check_extremes(data):
+    return (data[0] == data[1] and data[1] != data[2]) or (
+        data[4] == data[5] and data[3] != data[4]
+    )
+
+
+def check_inside(data, i):
+    return (
+        data[i] == data[i - 1] and data[i] != data[i + 1] and data[i - 2] != data[i]
+    ) or (data[i] == data[i + 1] and data[i] != data[i - 1] and data[i + 2] != data[i])
+
+
 def check_no_larger_group(data):
-    for i in range(6):
-        if i in [0, 1]:
-            if data[0] == data[1] and data[1] != data[2]:
-                return True
-        elif i in [4, 5]:
-            if data[4] == data[5] and data[3] != data[4]:
-                return True
-        else:
-            if (
-                data[i] == data[i - 1]
-                and data[i] != data[i + 1]
-                and data[i - 2] != data[i]
-            ):
-                return True
-            elif (
-                data[i] == data[i + 1]
-                and data[i] != data[i - 1]
-                and data[i + 2] != data[i]
-            ):
-                return True
-    return False
+    return any(
+        i in [1, 4]
+        and check_extremes(data)
+        or i not in [1, 4]
+        and check_inside(data, i)
+        for i in range(1, 5)
+    )
