ebdfa95a66bdd2403166cc8f9b5d0a64abe0b371
fabiogallotti <42519154+fabiogallotti@users.noreply.github.com>
2020-12-06 09:53:34 +0100

    2020 day6 (#48)

---
+++ src/year2020/day06/functions.py
@@ -0,0 +1,44 @@
+def preprocessing(data):
+    forms = [[]]
+    i = 0
+    for elem in data:
+        if elem != "":
+            forms[i].extend(elem)
+        else:
+            i += 1
+            forms.append([])
+
+    dict_forms = []
+    for form in forms:
+        dict_ = {}
+        for elem in form:
+            if elem in dict_:
+                dict_[elem] += 1
+            else:
+                dict_[elem] = 1
+        dict_forms.append(dict_)
+
+    return dict_forms
+
+
+def how_many_in_group(forms, data):
+    how_many = [0] * len(forms)
+    i = 0
+    for elem in data:
+        if elem != "":
+            how_many[i] += 1
+        else:
+            i += 1
+    return how_many
+
+
+def solve_problem(problem, forms, how_many=None):
+    if problem == 1:
+        return sum(len(elem) for elem in forms)
+    elif problem == 2:
+        return sum(
+            1
+            for i, elem in enumerate(forms)
+            for value in elem.values()
+            if value == how_many[i]
+        )
---
+++ tests/2020/test_2020day06.py
@@ -0,0 +1,40 @@
+from src.year2020.day06.functions import preprocessing, solve_problem, how_many_in_group
+
+
+def test_preprocessing():
+    data = ["abc", "", "a", "b", "c"]
+
+    assert preprocessing(data) == [{"a": 1, "b": 1, "c": 1}, {"a": 1, "b": 1, "c": 1}]
+
+
+def test_how_many_in_group():
+    data = ["abc", "", "a", "b", "c"]
+    forms = [{"a": 1, "b": 1, "c": 1}, {"a": 1, "b": 1, "c": 1}]
+
+    assert how_many_in_group(forms, data) == [1, 3]
+
+
+def test_solve_problem():
+    data = [
+        "abc",
+        "",
+        "a",
+        "b",
+        "c",
+        "",
+        "ab",
+        "ac",
+        "",
+        "a",
+        "a",
+        "a",
+        "a",
+        "",
+        "b",
+    ]
+
+    forms = preprocessing(data)
+    how_many = how_many_in_group(forms, data)
+
+    assert solve_problem(1, forms) == 11
+    assert solve_problem(2, forms, how_many) == 6
