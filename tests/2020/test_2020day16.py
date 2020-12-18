b6db64fb0310f9a7192c1a624c16655bd9668f0b
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2020-12-18 23:55:56 +0100

    2020 day16 part1

---
+++ tests/2020/test_2020day16.py
@@ -0,0 +1,20 @@
+from year2020.day16.functions import part_1
+
+EXAMPLE_1 = [
+    "class: 1-3 or 5-7",
+    "row: 6-11 or 33-44",
+    "seat: 13-40 or 45-50",
+    "",
+    "your ticket:",
+    "7,1,14",
+    "",
+    "nearby tickets:",
+    "7,3,47",
+    "40,4,50",
+    "55,2,20",
+    "38,6,12",
+]
+
+
+def test_part_1():
+    assert part_1(EXAMPLE_1) == 71
