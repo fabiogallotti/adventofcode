bc081d40bbbec23f67dec97630e7119a65b0bafe
fabiogallotti <fabiogallotti@users.noreply.github.com>
2021-12-02 22:37:35 +0000

    format code with black

--- tests/2021/test_2021day02.py
+++ tests/2021/test_2021day02.py
@@ -1,6 +1,13 @@
 from src.year2021.day02.functions import part_1, part_2
 
-DATA = [["forward", "5"], ["down", "5"], ["forward", "8"], ["up", "3"], ["down", "8"], ["forward", "2"]]
+DATA = [
+    ["forward", "5"],
+    ["down", "5"],
+    ["forward", "8"],
+    ["up", "3"],
+    ["down", "8"],
+    ["forward", "2"],
+]
 
 
 def test_single():
