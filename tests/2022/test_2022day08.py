33631fa5a4b418d1e30a47a21f2ab0c004c2ba2c
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2022-12-09 11:06:57 +0100

    2022 day08

---
+++ tests/2022/test_2022day08.py
@@ -0,0 +1,17 @@
+from src.year2022.day08.functions import part_1, part_2
+
+INPUT = [
+    ["3", "0", "3", "7", "3"],
+    ["2", "5", "5", "1", "2"],
+    ["6", "5", "3", "3", "2"],
+    ["3", "3", "5", "4", "9"],
+    ["3", "5", "3", "9", "0"],
+]
+
+
+def test_part_1():
+    assert part_1(INPUT) == 21
+
+
+def test_part_2():
+    assert part_2(INPUT) == 8
