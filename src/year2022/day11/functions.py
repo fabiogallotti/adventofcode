1b149662b25aef629bff94d9b2944a0ef75a3f83
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2022-12-12 08:44:17 +0100

    2022 day11

---
+++ src/year2022/day11/functions.py
@@ -0,0 +1,117 @@
+import contextlib
+import math
+
+
+def preprocessing(data):
+    data = [e.split() for e in data]
+
+    monkeys = {}
+    for e in data:
+        with contextlib.suppress(IndexError):
+            if "monkey" in e[0].lower():
+                monkey = int(e[1].strip(":"))
+                monkeys[monkey] = {}
+
+            if "starting" in e[0].lower():
+                s_it = [e.strip(",") for e in e[2:]]
+                s_it = [int(i) for i in s_it]
+                monkeys[monkey]["s_it"] = s_it
+
+            if "operation" in e[0].lower():
+                ope = [e[4], e[5]]
+                monkeys[monkey]["ope"] = ope
+
+            if "test" in e[0].lower():
+                monkeys[monkey]["test"] = [int(e[3])]
+
+            if "true" in e[1].lower():
+                monkeys[monkey]["test"].append(int(e[5]))
+
+            if "false" in e[1].lower():
+                monkeys[monkey]["test"].append(int(e[5]))
+    return monkeys
+
+
+def part_1(data):
+    monkeys = preprocessing(data)
+    for _ in range(20):
+        for monkey, value in monkeys.items():
+            ope = value["ope"]
+            test = value["test"][0]
+            for it in value["s_it"]:
+                if ope[0] == "*" and ope[1] == "old":
+                    new_it = it * it
+                elif ope[0] == "+" and ope[1] == "old":
+                    new_it = it + it
+                elif ope[0] == "*":
+                    new_it = it * int(ope[1])
+                elif ope[0] == "+":
+                    new_it = it + int(ope[1])
+
+                new_it = new_it // 3
+
+                if new_it % test == 0:
+                    true = value["test"][1]
+                    monkeys[true]["s_it"].append(new_it)
+                else:
+                    false = value["test"][2]
+                    monkeys[false]["s_it"].append(new_it)
+
+                monkeys[monkey]["s_it"] = monkeys[monkey]["s_it"][1:]
+
+                if "inspected" not in monkeys[monkey]:
+                    monkeys[monkey]["inspected"] = 1
+                else:
+                    monkeys[monkey]["inspected"] += 1
+
+    inspected = [monkeys[monkey]["inspected"] for monkey in monkeys]
+    inspected = sorted(inspected, reverse=True)
+
+    return inspected[0] * inspected[1]
+
+
+def part_2(data):
+    monkeys = preprocessing(data)
+    divisors = [monkey["test"][0] for monkey in monkeys.values()]
+    ops = [
+        int(monkey["ope"][1])
+        for monkey in monkeys.values()
+        if monkey["ope"][1] != "old"
+    ]
+
+    lcm = math.lcm(*divisors, *ops)
+
+    for _ in range(10000):
+        for monkey, value in monkeys.items():
+            ope = value["ope"]
+            for it in value["s_it"]:
+                if ope[0] == "*" and ope[1] == "old":
+                    new_it = it * it
+                elif ope[0] == "+" and ope[1] == "old":
+                    new_it = it + it
+                elif ope[0] == "*":
+                    new_it = it * int(ope[1])
+                elif ope[0] == "+":
+                    new_it = it + int(ope[1])
+
+                new_it = new_it % lcm
+
+                test = monkeys[monkey]["test"][0]
+                if new_it % test == 0:
+                    true = monkeys[monkey]["test"][1]
+                    monkeys[true]["s_it"].append(new_it)
+                else:
+                    false = monkeys[monkey]["test"][2]
+                    monkeys[false]["s_it"].append(new_it)
+
+                monkeys[monkey]["s_it"] = monkeys[monkey]["s_it"][1:]
+
+                if "inspected" not in monkeys[monkey]:
+                    monkeys[monkey]["inspected"] = 1
+                else:
+                    monkeys[monkey]["inspected"] += 1
+
+    inspected = [value_["inspected"] for value_ in monkeys.values()]
+    inspected = sorted(inspected, reverse=True)
+
+    return inspected[0] * inspected[1]
---
+++ tests/2022/test_2022day11.py
@@ -0,0 +1,39 @@
+from src.year2022.day11.functions import part_1, part_2
+
+INPUT = [
+    "Monkey 0:",
+    "  Starting items: 79, 98",
+    "  Operation: new = old * 19",
+    "  Test: divisible by 23",
+    "    If true: throw to monkey 2",
+    "    If false: throw to monkey 3",
+    "",
+    "Monkey 1:",
+    "  Starting items: 54, 65, 75, 74",
+    "  Operation: new = old + 6",
+    "  Test: divisible by 19",
+    "    If true: throw to monkey 2",
+    "    If false: throw to monkey 0",
+    "",
+    "Monkey 2:",
+    "  Starting items: 79, 60, 97",
+    "  Operation: new = old * old",
+    "  Test: divisible by 13",
+    "    If true: throw to monkey 1",
+    "    If false: throw to monkey 3",
+    "",
+    "Monkey 3:",
+    "  Starting items: 74",
+    "  Operation: new = old + 3",
+    "  Test: divisible by 17",
+    "    If true: throw to monkey 0",
+    "    If false: throw to monkey 1",
+]
+
+
+def test_part_1():
+    assert part_1(INPUT) == 10605
+
+
+def test_part_2():
+    assert part_2(INPUT) == 2713310158
