5784efcd8933e72cb4a5f838b4a5dbd558f990c4
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2022-12-16 22:28:52 +0100

    2022 day15

---
+++ src/year2022/day15/exercises.py
@@ -0,0 +1,9 @@
+from functions.read_input import read_input
+from inputs.path import PATH
+from .functions import part_1, part_2
+
+data = read_input(f"{PATH}/2022/day15.txt")
+
+print(f"First part: {part_1(data, 2000000)}")
+
+print(f"Second part: {part_2(data, 4000000)}")
