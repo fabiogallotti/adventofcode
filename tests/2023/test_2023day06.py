c7b1331cb37e36a1b852cb70f296c3e5ec7831ba
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2023-12-12 15:23:23 +0100

    feat: 2023 day 06

---
+++ tests/2023/test_2023day06.py
@@ -0,0 +1,14 @@
+from src.year2023.day06.functions import part_1, part_2
+
+EXAMPLE = [
+    "Time:      7  15   30",
+    "Distance:  9  40  200",
+]
+
+
+def test_part_1():
+    assert part_1(EXAMPLE) == 288
+
+
+def test_part_2():
+    assert part_2(EXAMPLE) == 71503
