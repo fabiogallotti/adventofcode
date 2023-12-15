15714d3f02c2848249d7fd2504cdea140908029e
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2023-12-16 00:49:51 +0100

    feat: 2023 day 11

---
+++ src/year2023/day11/functions.py
@@ -0,0 +1,56 @@
+from pydantic import BaseModel
+from itertools import combinations
+
+
+class Galaxy(BaseModel):
+    x: int
+    y: int
+
+
+def part_1(data, expansion):
+    galaxies = preprocessing(data, expansion)
+
+    return sum(
+        abs(galaxy_1.x - galaxy_2.x) + abs(galaxy_1.y - galaxy_2.y)
+        for galaxy_1, galaxy_2 in combinations(galaxies, 2)
+    )
+
+
+def part_2(data, expansion):
+    galaxies = preprocessing(data, expansion)
+
+    return sum(
+        abs(galaxy_1.x - galaxy_2.x) + abs(galaxy_1.y - galaxy_2.y)
+        for galaxy_1, galaxy_2 in combinations(galaxies, 2)
+    )
+
+
+def preprocessing(data, expansion):
+    galaxies = []
+    not_empty_rows = set()
+    not_empty_cols = set()
+    for i, row in enumerate(data):
+        for j, value in enumerate(row):
+            if value == "#":
+                galaxies.append(Galaxy(x=int(i), y=int(j)))
+                not_empty_rows.add(int(i))
+                not_empty_cols.add(int(j))
+
+    empty_rows = sorted(list(set(range(len(data))) - not_empty_rows))
+    empty_cols = sorted(list(set(range(len(data[0]))) - not_empty_cols))
+
+    for galaxy in galaxies:
+        new_galaxy_x = galaxy.x
+        new_galaxy_y = galaxy.y
+        for row in empty_rows:
+            if galaxy.x > row:
+                new_galaxy_x += expansion
+
+        for col in empty_cols:
+            if galaxy.y > col:
+                new_galaxy_y += expansion
+
+        galaxy.x = new_galaxy_x
+        galaxy.y = new_galaxy_y
+
+    return galaxies
---
+++ tests/2023/test_2023day11.py
@@ -0,0 +1,23 @@
+from src.year2023.day11.functions import part_1, part_2
+
+EXAMPLE = [
+    "...#......",
+    ".......#..",
+    "#.........",
+    "..........",
+    "......#...",
+    ".#........",
+    ".........#",
+    "..........",
+    ".......#..",
+    "#...#.....",
+]
+
+
+def test_part_1():
+    assert part_1(EXAMPLE, expansion=1) == 374
+
+
+def test_part_2():
+    assert part_2(EXAMPLE, expansion=9) == 1030
+    assert part_2(EXAMPLE, expansion=99) == 8410
