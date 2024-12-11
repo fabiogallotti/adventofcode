271eaeea2ea852c59c7f4aa1c609cac82b9f8bad
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2024-12-11 16:48:31 +0100

    feat: 2024 day12 - part1

---
+++ tests/2024/test_2024day12.py
@@ -0,0 +1,62 @@
+from year2024.day12.functions import part_1
+
+EXAMPLE_1 = [
+    "AAAA",
+    "BBCD",
+    "BBCC",
+    "EEEC",
+]
+
+EXAMPLE_2 = [
+    "OOOOO",
+    "OXOXO",
+    "OOOOO",
+    "OXOXO",
+    "OOOOO",
+]
+
+EXAMPLE_3 = [
+    "RRRRIICCFF",
+    "RRRRIICCCF",
+    "VVRRRCCFFF",
+    "VVRCCCJFFF",
+    "VVVVCJJCFE",
+    "VVIVCCJJEE",
+    "VVIIICJJEE",
+    "MIIIIIJJEE",
+    "MIIISIJEEE",
+    "MMMISSJEEE",
+]
+
+
+EXAMPLE_4 = [
+    "EEEEE",
+    "EXXXX",
+    "EEEEE",
+    "EXXXX",
+    "EEEEE",
+]
+
+
+EXAMPLE_5 = [
+    "AAAAAA",
+    "AAABBA",
+    "AAABBA",
+    "ABBAAA",
+    "ABBAAA",
+    "AAAAAA",
+]
+
+
+def test_part_1():
+    assert part_1(EXAMPLE_1) == 140
+    assert part_1(EXAMPLE_2) == 772
+    assert part_1(EXAMPLE_3) == 1930
+
+
+# def test_part_2():
+#     assert part_2(EXAMPLE_1) == 80
+#     assert part_2(EXAMPLE_2) == 436
+#     assert part_1(EXAMPLE_3) == 1206
+#     assert part_2(EXAMPLE_4) == 236
+#     assert part_2(EXAMPLE_5) == 368
