230182868a0a452e20124796fbfd62e0ec09da67
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2024-12-11 12:01:20 +0100

    feat: 2024 day11

---
+++ src/year2024/day11/exercises.py
@@ -0,0 +1,10 @@
+from functions.read_input import read_input
+from inputs.path import PATH
+
+from .functions import part_1, part_2
+
+data = read_input(f"{PATH}/2024/day11.txt")
+
+print(f"First part: {part_1(data)}")
+
+print(f"Second part: {part_2(data)}")
---
+++ tests/2024/test_2024day11.py
@@ -0,0 +1,11 @@
+from year2024.day11.functions import part_1, part_2
+
+EXAMPLE_1 = ["125 17"]
+
+
+def test_part_1():
+    assert part_1(EXAMPLE_1) == 55312
+
+
+def test_part_2():
+    assert part_2(EXAMPLE_1) == 65601038650482
