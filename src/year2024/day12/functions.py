eedabe8bec435b0e46f6f0bedbd227212f4e77d5
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2024-12-18 15:57:34 +0100

    feat: 2024 day12 - part2

--- src/year2024/day12/functions.py
+++ src/year2024/day12/functions.py
@@ -79,5 +79,61 @@ def part_1(data):
     return price
 
 
+def compute_sides(points):
+    sides = 0
+
+    for point in points:
+        u_point = Point(x=point.x + Direction.UP.value[0], y=point.y + Direction.UP.value[1])
+        l_point = Point(x=point.x + Direction.LEFT.value[0], y=point.y + Direction.LEFT.value[1])
+        ul_point = Point(x=point.x + Direction.UP.value[0], y=point.y + Direction.LEFT.value[1])
+
+        if u_point not in points:
+            same_edge = (l_point in points) and (ul_point not in points)
+            if not same_edge:
+                sides += 1
+
+        d_point = Point(x=point.x + Direction.DOWN.value[0], y=point.y + Direction.DOWN.value[1])
+        dl_point = Point(x=point.x + Direction.DOWN.value[0], y=point.y + Direction.LEFT.value[1])
+
+        if d_point not in points:
+            same_edge = (l_point in points) and (dl_point not in points)
+            if not same_edge:
+                sides += 1
+
+        if l_point not in points:
+            same_edge = (u_point in points) and (ul_point not in points)
+            if not same_edge:
+                sides += 1
+
+        r_point = Point(x=point.x + Direction.RIGHT.value[0], y=point.y + Direction.RIGHT.value[1])
+        ur_point = Point(x=point.x + Direction.UP.value[0], y=point.y + Direction.RIGHT.value[1])
+
+        if r_point not in points:
+            same_edge = (u_point in points) and (ur_point not in points)
+            if not same_edge:
+                sides += 1
+
+    return sides
+
+
 def part_2(data):
-    pass
+    map_dict = preprocessing(data)
+
+    seen = set()
+    regions = defaultdict(list)
+
+    for elem, points in map_dict.items():
+        for point in points:
+            if point not in seen:
+                new_region = check_around(point, points, seen)
+                regions[elem].append(new_region)
+
+    price = 0
+
+    for region in regions.values():
+        for points in region:
+            area = len(points)
+            sides = compute_sides(points)
+            price += area * sides
+
+    return price
