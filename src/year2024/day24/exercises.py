60c42b7675d9249d21c8f064cd20b6f4c465fbf8
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2024-12-24 23:00:53 +0100

    feat: 2024 day24 - part2

--- src/year2024/day24/exercises.py
+++ src/year2024/day24/exercises.py
@@ -7,4 +7,8 @@ data = read_input(f"{PATH}/2024/day24.txt")
 
 print(f"First part: {part_1(data)}")
 
-print(f"Second part: {part_2(data)}")
+print(f"Second part: {part_2(data)}")  # manually
+
+# a = ["z08", "mvb", "z18", "wss", "z23", "bmn", "rds", "jss"]
+# ",".join(sorted(a))
+# 'bmn,jss,mvb,rds,wss,z08,z18,z23'
