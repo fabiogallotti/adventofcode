d953772ee262d399aa63d1fb98a3fdbd203fe96a
Fabio Gallotti <fabio.gallotti.93@gmail.com>
2024-12-31 08:45:45 +0100

    feat: 2024 day21

---
+++ tests/2024/test_2024day21.py
@@ -0,0 +1,13 @@
+from year2024.day21.functions import part_1
+
+EXAMPLE_1 = [
+    "029A",
+    "980A",
+    "179A",
+    "456A",
+    "379A",
+]
+
+
+def test_part_1():
+    assert part_1(EXAMPLE_1) == 126384
